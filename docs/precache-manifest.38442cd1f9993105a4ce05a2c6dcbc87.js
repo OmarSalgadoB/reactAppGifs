2b1be99c0e31531860ae9ab5b69f06515b3c871a
Omar Salgado <omarsalgado37@gmail.com>
2020-07-27 22:59:29 -0500

    Se cambia el nombre de la carpeta a docs

---
+++ docs/precache-manifest.38442cd1f9993105a4ce05a2c6dcbc87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6568546acca170886a4ccaa3d9b7e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b4ea0b78af3d1ccc4a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4fd675aa51df549db4c1",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c5b4ea0b78af3d1ccc4a",
+    "url": "/static/js/2.775af3c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.775af3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd675aa51df549db4c1",
+    "url": "/static/js/main.4afdb316.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
